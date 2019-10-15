51168f4ab75bc30873ebacc6a31de8007cf0864d
Youval A <y.aberman@mail.utoronto.ca>
2019-10-15 12:29:38 -0400

    hw

--- group_analysis_skeleton.py
+++ group_analysis_skeleton.py
@@ -20,7 +20,9 @@ import shutil
 #
 testingrooms = ['A','B','C']
 for room in testingrooms:
-...
+    source= '/Users/uv/Documents/GitHub/ps2-UVab/testingroom'+room+'/experiment_data.csv'
+    destination= '/Users/uv/Documents/GitHub/ps2-UVab/rawdata/IATdata_Room'+room+'.csv'
+    shutil.copy(source,destination)
 
 
 #%%
@@ -29,22 +31,42 @@ for room in testingrooms:
 #
 data = np.empty((0,5))
 for room in testingrooms:
-...
-
-
+    temp = sp.loadtxt('/Users/uv/Documents/GitHub/ps2-UVab/rawdata/IATdata_Room'+room+'.csv',delimiter=',')
+    data = np.vstack([data,temp])
 #%%
 # calculate overall average accuracy and average median RT
 #
-acc_avg = ...   # 91.48%
-mrt_avg = ...   # 477.3ms
-
+acc_avg = np.mean(data[:,3])   # 91.48%
+mrt_avg = np.mean(data[:,4])   # 477.3ms
 
 #%%
 # calculate averages (accuracy & RT) split by stimulus using a for loop and an 
 # if statement. (i.e., loop through the data to make a sum for each condition, 
 # then divide by the number of data points going into the sum)
 #
-...
+acc_word_avg = 0.0
+mrt_word_avg = 0.0 
+acc_face_avg = 0.0
+mrt_face_avg = 0.0 
+
+for row in range(len(data)) : 
+    if data[row,1] == 1:
+        acc_word_avg+= data[row,3]
+        mrt_word_avg+= data[row,4]
+    elif data[row,1]==2: 
+        acc_face_avg+= data[row,3]
+        mrt_face_avg+= data[row,4]
+    else:
+        print('!!!row number '+str(row)+' missing!!!')    #make sure if there are NAs or others they will be flagged! 
+acc_word_avg/= sum(data[:,1] == 1)   #divide each sum by the number of rows that satisfy the condition! 
+mrt_word_avg/= sum(data[:,1] == 1) 
+acc_face_avg/= sum(data[:,1] == 2)
+mrt_face_avg/= sum(data[:,1] == 2)
+
+print("words: "+"acc:"+str(acc_word_avg)) #print each conditional mean 
+print("words: "+"mrt:"+str(mrt_word_avg))
+print("faces: "+"acc:"+str(acc_face_avg))
+print("faces: "+"mrt:"+str(mrt_face_avg))
 
 # words: 88.6%, 489.4ms   faces: 94.4%, 465.3ms
 
@@ -55,7 +77,8 @@ mrt_avg = ...   # 477.3ms
 # wp - white/pleasant, bp - black/pleasant
 # (hint: only one line of code is needed per average)
 #
-acc_wp = ...  # 94.0%
+data[:,3]
+acc_wp = np.mean(data[:,3])    # 94.0%
 acc_bp = ...  # 88.9%
 mrt_wp = ...  # 469.6ms
 mrt_bp = ...  # 485.1ms
